2674f15dcfedbf9cd249113f2e419923fb85659b
Fernando Álisson <fernando.alisson@ufpe.br>
2023-11-20 13:45:32 -0300

    fix

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -2,7 +2,6 @@ const { userServices } = require('../service/index');
 
 const getAllUsers = async (req, res) => {
   const users = await userServices.getAllUsers();
-
   return res.status(200).json(users);
 };
 
