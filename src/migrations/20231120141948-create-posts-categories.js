320b7afa93a176d7bfc93c34f1f2853901f16021
Fernando Álisson <fernando.alisson@ufpe.br>
2023-11-20 11:36:24 -0300

    Adicionando as migrations para as tabelas

---
+++ src/migrations/20231120141948-create-posts-categories.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('posts_categories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      post_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'blog_posts',
+          key: 'id'
+        },
+      },
+      category_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'categories',
+          key: 'id'
+        },
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('posts_categories');
+  }
+};
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,7 +4,7 @@ const { userController } = require('../controller/index');
 
 const userRouters = Router();
 
-userRouters.get('/', userController.getAllUsers);
+userRouters.get('/user', userController.getAllUsers);
 // Colocar as demais rotas aqui.
 
 module.exports = userRouters;
